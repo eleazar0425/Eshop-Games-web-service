e4d34a97a1b0533a56352aa6c50aab7f2e3b4537
Eleazar Estrella GB <eleazarestrella@Eleazars-iMac.local>
2019-02-15 13:24:31 -0400

    update web service lib

--- server.js
+++ server.js
@@ -1,15 +1,38 @@
-const SwitchEshop = require("./nintendo-switch-eshop")
+const SwitchEshop = require("nintendo-switch-eshop")
 var express = require('express')
 var app = express()
 
 var port = process.env.PORT  || 1337
 
+function isEmpty(obj) {
+    for(var key in obj) {
+        if(obj.hasOwnProperty(key))
+            return false;
+    }
+    return true;
+}
 
+app.get('/gamesAmerica', async function(req, res){
 
-app.get('/gamesAmerica', function(req, res){
-    SwitchEshop.getGamesAmerica(req.query.offset || 0, null).then(function(result, error){
-        res.json(result)
-    })
+    try {
+        var stillFetchingData = true
+        var results = []
+        var counter = 0
+
+        while(stillFetchingData) {
+            const data = await SwitchEshop.getGamesAmerica({shop: "all", limit: 200}, counter*200)
+            console.log("data received: "+data.length)
+            counter++
+            if (isEmpty(data)) {
+                stillFetchingData = false
+            }else {
+                results = results.concat(data)
+            }
+        }
+        res.send(results)
+    }catch (e) {
+        throw(e)
+    }
 })
 
 app.listen(port)
