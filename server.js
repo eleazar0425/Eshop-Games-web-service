be2a25badba46541d3f43b307ca0a3763421767f
Eleazar Estrella GB <eleazarestrella@Eleazars-iMac.local>
2019-03-09 12:26:46 -0400

    update cron job

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.listen(port)
 
 console.log('Server running at port:'+port);
 var cron = require('cron');
-var cronJob = cron.job('0 */12 * * * *', function(){
+var cronJob = cron.job('0 1,13 * * *', function(){
     repository.getGamesAmerica().then(function(results){
         results.forEach(game => {
             if(game.hasOwnProperty('sale_price')){
